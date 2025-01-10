336470b12c43b7582cccd772a9db984f06cf661f
heng30 <2238288979@qq.com>
2025-01-10 16:32:00 +0800

    [*] update normally

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 It's a demo that calling Go functions from Rust.
 
 ### How to use?
-- make run
-- make build
+- make run-on-linux
+- make run-on-macos
+- make run-on-windows
+
+### Note
+- unsupport call Go functions from shared lib on macos
+- unsupport call Go functions from static lib on windows
--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,5 +1,7 @@
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 use std::ffi::c_int;
 
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 extern "C" {
     fn GoAdd(a: c_int, b: c_int) -> c_int;
     fn SayHi();
@@ -14,17 +16,12 @@ fn main() {
         println!("Sum from Go: {}", sum);
     }
 
-    #[allow(unused_imports)]
-    #[allow(dead_code)]
     #[cfg(target_os = "windows")]
     {
-        use std::env;
-        use std::path::Path;
         use libloading::{Library, Symbol};
 
-        let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-        // let lib_path = Path::new(&out_dir).join("..").join("build").join(
-       let lib_path = Path::new(&out_dir).join("..").join(
+        let out_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let lib_path = std::path::Path::new(&out_dir).join("..").join(
             "libgo-shared.dll",
         );
 
@@ -37,8 +34,7 @@ fn main() {
 
         unsafe {
             let lib = Library::new(&lib_path).unwrap();
-            let sayhi_func: Symbol<unsafe extern "C" fn(u32, u32) -> u32> = lib.get(b"SayHi")
-                .unwrap();
+            let sayhi_func: Symbol<unsafe extern "C" fn()> = lib.get(b"SayHi").unwrap();
 
             sayhi_func(3, 5);
 
